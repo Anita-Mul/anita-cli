1a9b06cf01620dc0242552ea026197586265bd31
小可爱 <2659580957@qq.com>
2022-10-19 15:22:50 +0800

    add request to template

--- IMOOC-CLI-yan/packages/init/lib/index.js
+++ IMOOC-CLI-yan/packages/init/lib/index.js
@@ -1,7 +1,26 @@
 'use strict';
+const { log } = require('@imooc-cli-yan/utils');
+const getProjectTemplate = require('./getProjectTemplate');
 
-function init() {
-    console.log(111111111111111);
+async function init(options) {
+    try {
+        log.verbose('init', options);
+        const { templateList } = await prepare();
+        console.log(templateList);
+        log.verbose('template', templateName);
+    } catch (e) {
+        log.error('Error:', e.message);
+    }
+}
+
+async function prepare() {
+    const templateList = await getProjectTemplate();
+    if (!templateList || templateList.length === 0) {
+        throw new Error('项目模板列表获取失败');
+    }
+    return {
+        templateList,
+    };
 }
 
 module.exports = init;
--- IMOOC-CLI-yan/packages/utils/lib/index.js
+++ IMOOC-CLI-yan/packages/utils/lib/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 const log = require('./log');
+const request = require('./request');
 const npm = require('./npm');
 
 const Package = require('./Package');
 
 module.exports = {
   log,
+  request,
   npm,
   Package,
 };
